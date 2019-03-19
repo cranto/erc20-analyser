aa610657b13b9595da74bee40deab1f9fbcaf732
Nik Shevchenko <n.shevchenko@gmail.com>
2019-03-19 16:47:38 +0300

    Library initialization

---
+++ src/get-request.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @param {*} value
+ * function to get request 
+ */
+function getRequest(value) {
+    return value
+}
+
+export default getRequest;
---
+++ src/timestamp.js
@@ -0,0 +1,11 @@
+
+/**
+ * 
+ * @param {*} value
+ * function to convert human readable time to UNIX timestamp 
+ */
+function toTimestamp(value) {
+    return value
+}
+
+export default toTimestamp;
