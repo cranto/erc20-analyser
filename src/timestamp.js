6baf824834e05b1aff4ff52e315f86f00cdfb851
loeildes <shevchenko.nik@icloud.com>
2019-03-20 18:15:28 +0300

    Update comments

--- src/timestamp.js
+++ src/timestamp.js
@@ -2,10 +2,22 @@
 /**
  * 
  * @param {*} value
- * function to convert human readable time to UNIX timestamp 
+ * function to convert human readable time to UNIX timestamp
+ * 
+ * To start: ISO-8601
+ * To finish: UNIX timestamp
  */
 function toTimestamp(value) {
-    return value
+
+    let toUnix = function (value) {
+        return new Date(value)
+    }
+
+    if (typeof value === 'string') {
+        return toUnix(value)
+    } else {
+        return false
+    }
 }
 
 export default toTimestamp;
