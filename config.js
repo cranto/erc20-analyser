c8a32b9b7752d3f633c57e5b99d271885a88abd3
loeildes <shevchenko.nik@icloud.com>
2019-03-20 22:26:03 +0300

    Initial config

---
+++ config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * Config to work with a API
+ */
+
+let config = {};
+
+config.API = {
+    etherscan: 'your-api-token-of-etherscan.io',
+    coinmarketcap: 'your-api-token-of-coinmarketcap',
+    cryptocompare: 'your-api-token-of-cryptocompare'
+}
+
+config.URL = {
+    etherscan: 'https://api.etherscan.io',
+    coinmarketcap: 'https://coinmarketcap.com/api',
+    cryptocompare: 'https://min-api.cryptocompare.com'
+}
+
+export default config;
