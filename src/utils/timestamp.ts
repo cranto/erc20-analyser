5d1b17f488b4040ec7b9e175b7a835f48de943cb
loeildes <shevchenko.nik@icloud.com>
2019-03-21 20:12:27 +0300

    Update timestamp

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -4,16 +4,11 @@
  * @param {*} value
  * function to convert human readable time to UNIX timestamp
  * 
- * To start: ISO-8601
- * To finish: UNIX timestamp
+ * To start: ISO-8601 (2018-06-22T19:29:37.000Z)
+ * To finish: UNIX timestamp (1529695777000)
  */
 function ToTimestamp(value: string) {
-
-    let toUnix = function (value: string | number) {
-        return new Date(value)
-    }
-
-    return toUnix(value)
+    return Date.parse(value)
 }
 
 export default ToTimestamp;
