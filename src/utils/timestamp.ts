1cc0bfa36c949afd7a85238b365cf5458abd4b2e
loeildes <shevchenko.nik@icloud.com>
2019-03-23 17:03:35 +0300

    Update comment in timestamp

--- src/utils/timestamp.ts
+++ src/utils/timestamp.ts
@@ -3,11 +3,18 @@
  * 
  * @param {*} value
  * function to convert human readable time to UNIX timestamp
+ * Using function Date.parse() but if get parameter with date from another API to change this function
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse#Using_Date.parse()
+ * 
+ * TODO: abstract date or function to convert for correct format
  * 
  * To start: ISO-8601 (2018-06-22T19:29:37.000Z)
+ * 
+ * Date objects use a Unix Time Stamp, an integer value that is the number of milliseconds since 1 January 1970 00:00:00 UTC
  * To finish: UNIX timestamp (1529695777000)
  */
 function ToTimestamp(value: string) {
+
     return Date.parse(value)
 }
 
