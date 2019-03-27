d0ead8866f6dffba72ad02edbf592b7050e4c122
loeildes <shevchenko.nik@icloud.com>
2019-03-27 12:37:23 +0300

    Add return type

--- src/utils/check-address.ts
+++ src/utils/check-address.ts
@@ -2,6 +2,8 @@ let isEthAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/g)
 
 /**
  * Ethereum address validation
+ * @param {string} address
+ * @return {boolean}
  * 
  * This is a clone of Web3 utility (isAddress)
  * But this way is better because size of bundle is smaller
@@ -12,7 +14,7 @@ let isEthAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/g)
  * TODO: Test for regular expression
  */
 
-function CheckAddress(address: string) {
+function CheckAddress(address: string): boolean {
 
     if (isEthAddress.test(address)) {
         return true
