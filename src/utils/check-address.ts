939ebd80f6b7c9c61eb6d4f78c6996f44cf0f517
loeildes <shevchenko.nik@icloud.com>
2019-03-28 18:00:04 +0300

    Complete checker eth address

--- src/utils/check-address.ts
+++ src/utils/check-address.ts
@@ -1,7 +1,55 @@
-let isEthAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/g)
+import { SHA3 } from 'crypto-js'
 
 /**
- * Ethereum address validation
+ * Regular expression to validate ETH address
+ */
+let isEthAddress = new RegExp(/^0x[a-fA-F0-9]{40}$/g)
+
+function checkSumAddress(address: string): boolean {
+    
+    /**
+     * Hash the lowercase hexadecimal string from Step-2 using Keccak 256 algorithm.
+     * @param {string} value 
+     */
+    let sha3F = (value: string): string => {
+        return SHA3(value, {
+            outputLength: 256
+        }).toString()
+    }
+
+    if (address.toString().substring(0,2) !== '0x') {
+        return false
+    }
+    
+    /**
+     * Remove the 0x prefix of the hex address.
+     */
+    let addressR = address.replace('0x', '')
+
+    /**
+     * Convert the rest of the address to lower-case.
+     */
+    let addressHash = sha3F(addressR.toLowerCase())
+
+    /**
+     * Compare the obtained hash with the original hex address:
+     * Change the nth letter of hexadecimal address to uppercase if the nth bit of the obtained hash is greater than 7.
+     * Otherwise, the nth letter of the hexadecimal address should be lowercased.
+     */
+    for (let i = 0; i < 40; i++) {
+        if (
+            (parseInt(addressHash[i], 16) > 7 && addressR[i].toUpperCase() !== addressR[i]) ||
+            (parseInt(addressHash[i], 16) <= 7 && addressR[i].toLowerCase() !== addressR[i])
+        ) {
+            return false
+        }
+    }
+
+    return true
+}
+
+/**
+ * Ethereum addres validation
  * @param {string} address
  * @return {boolean}
  * 
@@ -18,9 +66,11 @@ function CheckAddress(address: string): boolean {
 
     if (isEthAddress.test(address)) {
         return true
+    } else if (checkSumAddress(address)) {
+        return true
+    } else {
+        return false
     }
-
-    return false
 }
 
 export default CheckAddress;
