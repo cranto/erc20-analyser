32b611d06947ea5a4e0e3737951bfe6acc5049c0
loeildes <shevchenko.nik@icloud.com>
2019-04-08 18:09:55 +0300

    Update test for timestamp

--- src/utils/__tests__/timestamp.test.js
+++ src/utils/__tests__/timestamp.test.js
@@ -1,4 +1,11 @@
 it('Check correct timestamp', () => {
-    const ToTimestamp = require('../timestamp.ts').default;
-    expect(ToTimestamp('2018-06-22T19:29:37.000Z')).toBe(1529695777000);
+    const ToTimestamp = require('../timestamp.ts');
+    expect(ToTimestamp.ToUnix('2018-06-22T19:29:37.000Z')).toBe(1529695777000);
+});
+
+it('Check incorrect timestamp', () => {
+    const ToTimestamp = require('../timestamp.ts');
+    expect(() => {
+        ToTimestamp.ToUnix('2018-06-22T1')
+    }).toThrow()
 });
