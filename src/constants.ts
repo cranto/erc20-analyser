e4a0bd1b518e7b0b437737c8c6089e9d941c5457
loeildes <shevchenko.nik@icloud.com>
2019-05-17 18:06:02 +0300

    Adding constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,24 @@
 /**
  * Etherscan.io constants
  */
-export const ETHERSCAN_API_KEY = ''
+
+export const ETHERSCAN_API_KEY = 'etherscanApiKey';
+
+export const ETHERSCAN_API = 'https://api.etherscan.io/api?module=';
+export const ETHERSCAN_API_ACCOUNT = 'account&';
+export const ETHERSCAN_API_BALANCE = '&action=balance&';
+export const ETHERSCAN_API_TXLIST = '&action=tokentx';
+export const ETHERSCAN_API_ADDRESS = '&address=';
 
 /**
  * Coinmarketcap.com constants
  */
-export const COINMARKETCAP_API_KEY = ''
+export const COINMARKETCAP_API_KEY = '';
 
 /**
  * Cryptocompare.com constants
  */
-export const CRYPTOCOMPARE_API_KEY = ''
+
+export const CRYPTOCOMPARE_API_KEY = 'cryptocompareApiKey';
+
+export const CRYPTOCOMPARE_API = 'https://min-api.cryptocompare.com/data/';
