dd3b67607b40df5773eaa695e42ab6eee5b579bd
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-05-23 18:02:58 +0300

    Remove API constants

--- src/constants.ts
+++ src/constants.ts
@@ -2,8 +2,6 @@
  * Etherscan.io constants
  */
 
-export const ETHERSCAN_API_KEY = 'etherscanApiKey';
-
 export const ETHERSCAN_API = 'https://api.etherscan.io/api?module=';
 export const ETHERSCAN_API_ACCOUNT = 'account&';
 export const ETHERSCAN_API_BALANCE = '&action=balance&';
@@ -11,14 +9,7 @@ export const ETHERSCAN_API_TXLIST = '&action=tokentx';
 export const ETHERSCAN_API_ADDRESS = '&address=';
 
 /**
- * Coinmarketcap.com constants
- */
-export const COINMARKETCAP_API_KEY = '';
-
-/**
  * Cryptocompare.com constants
  */
-
-export const CRYPTOCOMPARE_API_KEY = 'cryptocompareApiKey';
 
 export const CRYPTOCOMPARE_API = 'https://min-api.cryptocompare.com/data/';
