8e791817421fe667d58d18b66693e1dd1fb4d06f
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-05-24 23:46:22 +0300

    Adding CoinGecko constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 /**
  * Etherscan.io constants
  */
-
 export const ETHERSCAN_API = 'https://api.etherscan.io/api?module=';
 export const ETHERSCAN_API_ACCOUNT = 'account&';
 export const ETHERSCAN_API_BALANCE = '&action=balance&';
@@ -11,5 +10,10 @@ export const ETHERSCAN_API_ADDRESS = '&address=';
 /**
  * Cryptocompare.com constants
  */
-
 export const CRYPTOCOMPARE_API = 'https://min-api.cryptocompare.com/data/';
+
+/**
+ * CoinGecko.com constants
+ * Documentation: https://www.coingecko.com/api/documentations/v3#/
+ */
+export const COINGECKO_API = 'https://api.coingecko.com/api/v3/';
