25329b2e700e69d383b3092c9e67e329b8e2d1fd
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-06-06 21:34:20 +0300

    Add wrapper request to index

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,8 +1,8 @@
 import * as CheckAddress from './check-address';
 import * as ToTimestamp from './timestamp';
 import ThrowError from './throw-error';
-import { Request, Queue } from './request';
+import { Request, Queue, WrapperRequest } from './request';
 import { Configuration } from './configuration';
 import * as Checkers from './utilities';
 
-export { CheckAddress, ToTimestamp, ThrowError, Request, Configuration, Checkers, Queue };
+export { CheckAddress, ToTimestamp, ThrowError, Request, Configuration, Checkers, Queue, WrapperRequest };
