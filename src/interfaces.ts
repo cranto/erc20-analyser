71a3df745010d1ebec3f0d7ac47f049389c240d8
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-06-06 21:34:58 +0300

    Adding new interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,5 +1,13 @@
 export type EthAddress = string;
 
+export interface IEtherscanRequest {
+  data: {
+    status: string;
+    message: string;
+    result: object;
+  };
+}
+
 export interface IPriceToken {
   /** Token abbreviation */
   tokenSymbol: string;
