004055c64da07d23e4ac076a60c87cc64f8a186e
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-06-06 21:35:15 +0300

    Remove old tests

--- src/core/__tests__/etherscan.test.ts
+++ src/core/__tests__/etherscan.test.ts
@@ -1,7 +1,1 @@
 import { GetCurrentEthBalance } from '../etherscan';
-
-it('Get current eth balance', async () => {
-  expect.assertions(1);
-  const data = await GetCurrentEthBalance('0xd1220a0cf47c7b9be7a2e6ba89f429762e7b9adb');
-  expect(data).toEqual(125.026906038826337316);
-});
