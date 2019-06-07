30bc6f8e1c6c0d6d3582a6e6d71cd02f42ca3262
Nik Shevchenko <shevchenko.nik@icloud.com>
2019-06-07 19:50:03 +0300

    Default test for cryptocompare

--- src/core/__tests__/cryptocompare.test.ts
+++ src/core/__tests__/cryptocompare.test.ts
@@ -1,1 +1,9 @@
 import { GetPriceToken, GetCurrentPriceToken } from '..';
+
+it('Cryptocompare function – GetPriceToken, default', async () => {
+  expect.assertions(1);
+
+  const data = await GetPriceToken({ tokenSymbol: 'BNB', timestamp: '1555505512', toConvert: 'ETH' }, '');
+
+  expect(data).toEqual(0.1175);
+});
