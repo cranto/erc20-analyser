4aa1345ed5fdee61f2014b741903e04db7840f46
Nik <shevchenko.nik@icloud.com>
2024-04-26 12:55:53 +0200

    Minor changes of finance functions

--- src/core/finance.ts
+++ src/core/finance.ts
@@ -3,21 +3,19 @@ import { IROIValues } from '../interfaces';
 /**
  * Return on Investment (ROI)
  * ROI is a ratio between the net profit and cost of investment resulting from an investment of some resources
- * @param {object} data
- * @returns {number}
  */
 export function calculateROI(data: IROIValues): number {
   /**
    * ROI = (Current value of investment - cost of investments) / cost of investment
    */
-  const result = ((data.cvoi - data.coi) / data.coi) * 100;
-
-  return result;
+  return ((data.cvoi - data.coi) / data.coi) * 100;
 }
 
 /**
  * Sharpe ratio
- * Sharpe ratio is a way to examine the performance of an investment by adjusting for its risk.
+ * The Sharpe ratio compares the return of an investment (ROI) with its risk.
+ *
+ * https://www.investopedia.com/terms/s/sharperatio.asp
  *
  * @param data
  * @returns {any}
