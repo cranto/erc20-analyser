e0ad72ded65abd4949e90b2b9f981b80582cba61
Nik <shevchenko.nik@icloud.com>
2024-04-26 13:11:47 +0200

    Finance converted to class

--- src/core/finance.ts
+++ src/core/finance.ts
@@ -1,25 +1,34 @@
-import { IROIValues } from '../interfaces';
+interface Roi {
+  cvoi: number // Current value of investment
+  coi: number // Cost of investment
+}
 
 /**
  * Return on Investment (ROI)
  * ROI is a ratio between the net profit and cost of investment resulting from an investment of some resources
  */
-export function calculateROI(data: IROIValues): number {
+export function calculateROI(data: Roi): number {
   /**
    * ROI = (Current value of investment - cost of investments) / cost of investment
    */
   return ((data.cvoi - data.coi) / data.coi) * 100;
 }
 
-/**
- * Sharpe ratio
- * The Sharpe ratio compares the return of an investment (ROI) with its risk.
- *
- * https://www.investopedia.com/terms/s/sharperatio.asp
- *
- * @param data
- * @returns {any}
- */
-export function calculateSharpeRatio<T>(data: T): T {
-  return data;
+
+export class Finance {
+  /**
+   * Return on Investment (ROI)
+   * ROI is a ratio between the net profit and cost of investment resulting from an investment of some resources
+   */
+  public calculateROI(
+    roi: Roi
+  ): number {
+    return ((roi.cvoi - roi.coi) / roi.coi) * 100
+  }
+
+  /**
+   * TODO: Implement Sharpe Ratio
+   * The Sharpe ratio compares the return of an investment (ROI) with its risk.
+   * https://www.investopedia.com/terms/s/sharperatio.asp
+   */
 }
